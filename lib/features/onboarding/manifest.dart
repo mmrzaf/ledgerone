0baaae3deda8cba7beb005f6b771ac841083a090
Mohammadreza-Farkhondeh <mohammr.farkhonde12@shairf.edu>
2025-11-19 02:32:25 +0330

    chore: update feature manifests with new routes

--- lib/features/onboarding/manifest.dart
+++ lib/features/onboarding/manifest.dart
@@ -1,10 +1,16 @@
 class OnboardingManifest {
   static const String routePath = '/onboarding';
   static const String routeId = 'onboarding';
-  
+
+  static const List<String> guards = [];
+
   static const List<String> events = [
     'onboarding_view',
     'onboarding_complete',
     'onboarding_skip',
   ];
+
+  static bool matchesDeepLink(Uri uri) {
+    return uri.path == routePath;
+  }
 }
